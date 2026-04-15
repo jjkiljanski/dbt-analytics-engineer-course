43a7478b64b549319391cc5a606ddc0bcb1c8b97
Jan Kiljanski <jan.jozef.kiljanski@gmail.com>
2026-04-15 10:49:33 +0200

    Added conditional "," formatting based on the loop.last variable to int_orders__pivoted.sql.

--- models/intermediate/int_orders__pivoted.sql
+++ models/intermediate/int_orders__pivoted.sql
@@ -1,15 +1,18 @@
+{% set payment_methods = ['coupon', 'credit_card', 'gift_card', 'bank_transfer'] %}
+
 with payments as
 (
     select * from {{ref('stg_stripe__payments')}}
 ),
 pivoted as (
     select
-        order_id,
-        {% set payment_methods = ['coupon', 'credit_card', 'gift_card', 'bank_transfer'] %}
-        
+        order_id,        
         {% for method in payment_methods %}
-            sum (case when payment_method = '{{ method }}' then amount else 0 end) as {{ method }}_amount,
-        {% endfor %}
+        sum (case when payment_method = '{{ method }}' then amount else 0 end) as {{ method }}_amount
+        {%- if not loop.last -%}
+            ,
+        {%- endif -%}
+        {%- endfor -%}
     from payments
     group by order_id
 )
